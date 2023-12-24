8af63872d8ef6b1c1f3468d77a43133140337b5f
yukimaru73 <ekxmekxm@gmail.com>
2023-12-24 09:12:12 +0900

    Update Lua library paths and modify Sonar Ping Time variable

--- MFM_Sorner.lua
+++ MFM_Sorner.lua
@@ -26,7 +26,7 @@ require("Libs.PhysicsSensorLib")
 	29: Linear Velocity Abs
 	30: Angular Velocity Abs
 	----------------------------------Misc
-	31: Sonar Ping Time
+	31: Sonar Ping Time Base
 ]]
 
 ---Input(Bool)
@@ -69,8 +69,12 @@ EXEPTION_RANGE = property.getNumber("Sonar Exeption Range")
 PHS = PhysicsSensorLib:new()
 
 ---その他
-TICKS_TO_METER = 1480 / 60 / 2 -- 1480m/s / 60tick/s / 2
+TIME_BASE = -1
+TICKS2METER = 1480 / 60 / 2 -- 1480m/s / 60tick/s / 2
+RAD2TURN = 1 / (2 * math.pi) -- 1 / (2π)
 TURN2RAD = 2 * math.pi
+
+TIME = TIME_BASE
 
 
 --メインループ
@@ -78,20 +82,91 @@ function onTick()
 	--更新
 	PHS:update(17)
 	local pingStatus = input.getBool(9)
-	local pingTime = input.getNumber(31)
+	TIME_BASE = input.getNumber(31)
+	TIME = pingStatus and (TIME + 1) or TIME_BASE
 
-	if pingStatus then
+	if pingStatus and TIME > 0 then
 		--アクティヴモード
+		--debug.log("TST|| " .. TIME)
+
+		--ターゲットの取得
+		---@type Vector3[]
+		local targets = {}
+		local targetsGlobal = {}
+		for i = 1, 8 do
+			local isFound = input.getBool(i)
+			if not isFound then break end
+
+			--距離、方位角、仰角の算出
+			local distance, azimuth, elevation =
+				TIME * TICKS2METER,
+				input.getNumber(i * 2 - 1) * TURN2RAD,
+				input.getNumber(i * 2) * TURN2RAD
+
+			--指定距離以上の場合のみターゲットとして登録
+			if distance > EXEPTION_RANGE then
+				local target = Vector3:newFromPolar(distance, azimuth, elevation)
+				targets[i] = PHS:rotateVector(target, true) --グローバル座標系(ソナー原点)のデータ
+				targetsGlobal[i] = targets[i]:add(PHS:getGPS()) --GPSグローバル座標系のデータ
+			end
+		end
+
+		--角度、距離、高度の算出
+		local params = {}
+		for i = 1, #targets do
+			local azimuth, distance, altitude =
+				-targets[i]:getAzimuth() * RAD2TURN, --コンパスと同じ規格の角度情報
+				targets[i]:getMagnitude(), --相対距離
+				targetsGlobal[i][2] --深度(Y座標)
+				
+
+			params[i] = {azimuth, distance, altitude}
+		end
+
+		--出力の設定
+		for i = 1, 8 do
+			local azimuth, distance, altitude = table.unpack(params[i] or {})
+
+			output.setBool(i, azimuth ~= nil) --発見
+			output.setNumber(i, not azimuth and 0 or azimuth) --コンパスと同じ規格の角度情報
+			output.setNumber(i + 8, not distance and 0 or distance) --相対距離
+			output.setNumber(i + 16, not altitude and 0 or altitude) --深度(Y座標)
+		end
 
 	else
 		--パッシヴモード
+
+		--ターゲットの取得
+		---@type Vector3[]
 		local targets={}
 		for i = 1, 8 do
-			local azimuth, elevation =
-				input.getNumber(i * 2 - 1),
-				input.getNumber(i * 2)
-			
 			local isFound = input.getBool(i)
+			if not isFound then break end --見つからない場合は終了
+
+			--方位角、仰角の算出
+			local azimuth, elevation =
+			input.getNumber(i * 2 - 1) * TURN2RAD,
+			input.getNumber(i * 2) * TURN2RAD
+			
+			
+			local target = Vector3:newFromPolar(1, azimuth, elevation) --ローカル座標の計算(仮で距離1mとして計算)
+			targets[i] = PHS:rotateVector(target, true) --グローバル座標系(ソナー原点)のデータ
+		end
+
+		--角度の算出
+		local targetAzimuths = {}
+		for i, v in ipairs(targets) do
+			local azimuth = -v:getAzimuth() * RAD2TURN --コンパスと同じ規格の角度情報
+
+			targetAzimuths[i] = azimuth
+		end
+
+		--出力の設定
+		for i = 1, 8 do
+			local azimuth = targetAzimuths[i]
+
+			output.setBool(i, azimuth ~= nil) --発見
+			output.setNumber(i, not azimuth and 0 or azimuth) --コンパスと同じ規格の角度情報
 		end
 	end
 
